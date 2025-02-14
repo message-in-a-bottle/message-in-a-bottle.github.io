a013ff5ee148c684ca1e8cf39fa6ac820f8a501e
offic <officiallyeklipse@gmail.com>
2025-02-14 11:52:53 -0500

    WIP

---
+++ script.js
@@ -0,0 +1,124 @@
+//All the poetry!
+const pages = [
+    "hey, you may be wondering what this is (i did just send you a strange looking website after all).<br>i can’t tell you outright yet. but you know it’s from me (lin.)<br>so, let’s play a game.<br>there are 3 poems here. each one is written for you, about you. i’ve been working on them for a few weeks now, and i want you to follow these instructions.<br><br>1. when you’re playing this game, do <b>not</b> analyze these poems! i know that’s what you do with poetry, i know you love to do it. but i want you to focus on the writing from a solely reading-just-to-read viewpoint. for now, at least. when the game is done, go crazy. but there’s a reason i’m saying this. <br><br>2. each of these poems has part of a message i’ve encrypted. i’ve put detailed instructions on how to decode the message at the end of the poem, so you can read them, go back to the poem, and decode it. <br><br>3. decoding these messages will be extremely easy. expect that. there’s a reason i’ve made them easy.  <br><br>4. when you’re done reading the poem, type the words you decoded into the <b>indicated text section.</b> no punctuation, only spaces. don’t worry about capitalization- it isn’t case sensitive. <br><br>5. i hope you like this! <3<br><br><br>okay, that’s all from me. can’t wait to see what you think.<br><br><br>                     <b>- lin</b><br><br><br>the answer to each of the following questions gives you a <b>number</b>. for example, if the question is <b>“how many letters are in the word “four”</b>, the answer would be: <b>4.</b> in the end, you’ll get <b>5 numbers.</b> they should be in <b>ascending order.</b> for example, (3, 12, 31, 43, 52). each of these numbers <b>corresponds to a word in the poem.</b> for example, if the text says, <b>“i watch the trees sway”</b>, and the number given was <b>“4”</b>, it would <b>correspond to the word “trees”</b>. find the words in the <b>same order as the numbers are given</b>- and enter them into the text box below, <b>spaced with only 1 space between them, with no punctuation.</b> the input is not case sensitive. good luck!<br><b>questions.</b><br><br><b>1. the second number of the fibonacci sequence</b> <br> <br><br><b>2. atomic number of element samarium.</b> <br> <br><br><b>3. number of minutes in an hour and fifteen minutes.</b> <br> <br><br><b>4. number of keys on a standard piano.</b> <br> <br><br><b>5.number of bones in the human body minus the atomic number of californium. </b><br><br><br><b>scintillation</b><br><br><br>i wanted to be born a star<br>but i could only be your stovelight.<br>dying too soon, flickering too early, i buzz throughout the night, noise permeating like EM fields into the empty living room.<br>burning, slowly, fizzing like a firework, burning like all good things do.<br>burning as i watch you flaring away too.<br>silent conflagration that you are. i think of how many alexandrias it would take to match your eyes, and i know that if i was a star and you were one too like in some past life, binary and forever trapped in orbit when i blinked my eyes- ever so slowly at you,<br>open and shut singing<br>.. / .-.. --- ...- . / -.-- --- ..-<br>the earthlings would look up with their telescopes, with words like \"cepheid variable\" and \"scintillation\"<br>to justify why i softened my gaze.<br>but here, but now, we are feeble and weak<br>and yet you still sit by me and linger<br>and your eyes wander to the clock at 11:11 and i get to see them shut for just a moment.<br>and i get to see you hold your breath, like you're watching something fleeting and above the clouds.<br>and i know stars don't get this luxury.", 
+    "<b>leucanthemum</b><br><br><br><b>questions, set 2</b> <br><b>1. there are 2 words for this one. when you solve for the word, just take the word and the one after it. this number corresponds to the number of years since the last ice age (according to the wikipedia page for “Last Glacial Period”). take the latter number in the time range given there, and divide it by 100. <br> <br><br><b>2. number of vertebrae in a human spine multiplied by the number of moons orbiting pluto.</b> <br> <br><br><b>3. take only the second letter of this word. the number of hours in a week, plus the number of hydrogen atoms in one molecule of water.</b> <br> <br><br><b>4. only the first 6 letters of this word. this word corresponds to the number of possible partitions of the number 15. </b><br><br><br>there's a kind of flower,<br>the oxeye daisy, that<br>crops up on every patch of canadian grass in the may june juncture.<br>wedged between layers of thick toronto concrete, conscientious roots lodged in every gap like fingers<br>pressed into collarbones, tangled with dimes and<br>cigarette butts.<br>leucanthemum, sudden dawn of thought.<br>if you notice anything, it only leads you to notice more.<br>i've been taking pages from your book lately, learning to crease the corners of your mind.<br>descry is what's learnt behind fallen lips.<br>and a moving pen and mine has not been lulled since<br>the moment we spoke.<br>i map out the undergrowth with you,<br>jet-black curls tangled in your leaves, hoping we find<br>more than mere consonance hidden in the topsoil.<br>marguerite, in the wind.<br>the clock keeps me away,<br>but once you and i make it till tommorow,<br>we will buy a slice of the world.<br>we will eat it whole, using our forks just enough to seem civilized.<br>and as we lay against the earth and it carries us to friendlier fires,<br>promise me<br>you'll leave the lines etched into the pavement.",
+    "<b>nhớ (negative twenty degrees)</b><br><br><br><b>questions, set 3</b> <br><b> gonna be totally honest here. i’m anticipating you seeing the last page, so this is going to be the shortest puzzle. 3 consecutive words- once you solve for the first, take the 2 words after it. this word corresponds to 11^2. if you get the word “you”, it’s the 2 words before, not the 2 words after.<br><br><br>in vietnamese, the word for missing someone <br>and remembering them is the same (nhớ).  <br>today, i saw the geese flying south for winter. <br>v-shaped. i wondered how the clouds felt<br>seeing the crease of your eyes against the pale grey-blue actinoform <br>morning, northwest, -20° and counting. <br> <br>i tell myself, it cannot be your fault <br>that you say hello and in my mind, each time <br>you dip a finger in a lake of my heart <br>the ripples go on and on and on and on and <br>remember how i tried to explain diffraction and interference <br>do you think it works with people? <br>you speak and the wavelength shifts <br>do you feel the convergence? <br>you just said “goodmorning”.<br>i misheard it as “i love you.” <br> <br>if the earth suddenly stopped spinning- <br>(this is not a poet’s line, i’m telling you about orbital velocity) <br>the moon would start to move towards it <br>and it would eventually cause earth to start rotating again, at the same rate <br>tidal locked, gravity tugs interstellar bodies to hold each other the same way we entwine fingers. <br>see that line? like the prime meridian on your palms <br> it says we are going to be together <br>for the longest of times. <br>i made that up. i just wanted to hold your hand. <br> <br>i forgot how to write poems. i try to work, but <br>i look at you and all my textbooks read the same 3 words<br>if to miss means to remember, you’ve been absent from me since the moment we met.",
+    "hey, deb. i guess if you’re here, you’ve finished the puzzle. if you haven’t already figured out the less-than-subtle message, it’s <b>i think i like you- more than as a friend- i love you</b>. which is…in its own right, kind of stupid. let’s talk about why. you can <b>love</b> friends. we’ve said “i love you” to each other countless times. and i know this. and we don’t need to be more than friends. in a way that term is especially stupid- it makes it seem like friends- like what we are- isn’t enough, like it’s not a good enough thing to be. and i know how wrong that is. i love you, i love the way we are, i love texting you every morning and smiling whenever i hear you talk and overall just being in your presence. and i guess that could be a friendship thing. but, see, the thing is- i don’t see you as a “friend”. of course, i do, i love you the same way i love the rest of my friends- but you’re different in the most insane of ways. i love my friends, but i hear your voice and i feel home. i feel like your hair is what my hands are meant to run through, like your eyes are a haven for my gaze, like my palms are supposed to be fitted against yours like 2 halves of an orange. and that’s different, because i only feel that way about you, only you. i don’t know if \"more than a friend\" is the correct way to describe that- if it makes sense to you- but it’s how i feel about you, i think. and i don't just feel excited. you feel warm, inviting. i’ve had crushes, sure, but they feel like i have a supernova trapped in my chest. you feel like a fireplace i’m sitting by. and i want nothing more, nothing more than to lie down by you and bask in your light for all eternity, to stay in your warmth as we lie on the forest floor and count the stars together. hey, maybe “more than friends” is a shitty way to say that. maybe this is a shitty confession. it probably is, if i’m being honest with you. but i needed to talk about it- to talk about you, how you make me feel- and i’m a total coward, so i’m doing it through this. i just want you to know i’m currently and probably permanently caught up in loving you like i haven’t loved anyone else for a long time. this isn’t a bad thing- doesn’t have to be one. even if you don’t feel the same. you aren’t obligated to say, do, talk about this ever again if you don’t want to. but just know, there’s a corner of my heart, doctor who on blu-ray, bookshelf with our initials carved and it’s full of prehistory and paleontology encyclopedias, little jars with rocks and moss, and small artpieces plastered all over the walls. i’ve put fairy lights up, candles, a heater better than the one in your dorm- and it’s all for you. all ready for you if you ever want it. and i’ll always be waiting there. that’s all i want you to take away from this, if nothing else, okay? no expectations, no poems, nothing. just that i love you more than you know, more than you’ll ever know, more than words can convey and kisses can tell and eyes can whisper. and hey, all that love is waiting for you. if you want it. only if you want it.</b><br><br><br><b>lin <3</b>"
+]
+
+//The answers for each page
+const answers = [
+    "i think i like you",
+    "b",
+    "c"
+]
+
+/* ----- Initialization section ----- */
+// Retrieve the page the user left at, if one does not exist go to first page
+var currentPage = localStorage.getItem("currentPage") != null ? Number(localStorage.getItem("currentPage")) : 0;
+
+const flashTime = 600;
+
+var buttonAllowed = true;
+
+var inputField = document.getElementById("input-field");
+var inputButton = document.getElementById("input-button");
+var poemDisplay = document.getElementById("poem");
+var pageNumberDisplay = document.getElementById("page-number");
+var inputContainer = document.getElementById("input-container");
+
+inputField.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        buttonClicked();
+    }
+});
+
+goToPage(currentPage);
+/* ----- End initialization section ----- */
+
+function goToPage(page) {
+    if (page >= 4 || page < 0) {
+        currentPage = 0;
+    } else {
+        currentPage = page;
+    }
+    window.localStorage.setItem("currentPage", currentPage);
+    console.log("Went to page: " + currentPage);
+
+    inputField.value = "";
+    buttonAllowed = true;
+    // Set the text in the main poem element to the new page's text, if it exists
+    poemDisplay.innerHTML = pages[currentPage] != undefined ? pages[currentPage] : "Whoops, the text that's supposed to be here is missing!";
+    pageNumberDisplay.innerHTML = currentPage + 1;
+
+    setTimeout(() => {
+
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        });
+
+        if (page == 3) { 
+            inputField.setAttribute("placeholder", "navigate to page number (0 to 2)");
+        } else {
+            inputField.setAttribute("placeholder", "type here");
+        }
+
+    }, 200) // Add a slight delay before scrolling up so the screen doesn't try to scroll while the new page's text changes where everything is instantly, to avoid a chaotic-looking transition
+}
+
+function buttonClicked() {
+    // To prevent the user from using the button more times while going through its animation, because we want the input text to disappear after the transition for aesthetic reasons
+    if (!buttonAllowed) {
+        return;
+    }
+
+    var inputText = inputField.value.toLowerCase();
+    
+    var isCorrect;
+
+    console.log("Input: \"" + inputText + "\"");
+
+    if (currentPage == 3) {
+
+        isCorrect = true;
+        setTimeout(() => { goToPage(Number(inputText)) }, flashTime);
+
+    } else if (inputText == "reset") {
+
+        setTimeout(() => { goToPage(0) }, flashTime);
+        isCorrect = false;
+        console.log("Reset");
+
+    } else if (inputText == answers[currentPage] || inputText == "adminkey") {
+
+        isCorrect = true;
+        buttonAllowed = false;
+        inputText = "";
+        setTimeout(() => { goToPage(currentPage + 1) }, flashTime);
+        console.log("Correct input");
+
+    } else {
+
+        isCorrect = false;
+        console.log("Incorrect input");
+
+    }
+
+    flashInput(isCorrect);
+}
+
+function flashInput(isCorrect) {
+    var styleClassName;
+    if (isCorrect) {
+        styleClassName = "flash-green";
+    } else {
+        styleClassName = "flash-red";
+    }
+
+    inputButton.classList.add(styleClassName);
+    setTimeout(() => {
+        inputButton.classList.remove(styleClassName);
+    }, flashTime - 200) // #input-button's CSS transition rule value, hardcoded because who cares
+}
